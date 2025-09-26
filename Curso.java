939b3eea3a2e00ef84f850c34974ab69ad774197
Josue Madueño <josue4500@outlook.es>
2025-09-25 20:42:43 -0500

    Codigo de manguito añadido, solucionado problemas de compatibilidad

--- Curso.java
+++ Curso.java
@@ -1,27 +1,49 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Curso {
+    private Scanner sc = new Scanner(System.in);
+    private String codigo;
     private String nombre;
+    private Docente docente; 
 
-    // Constructor que pide el nombre al crear el curso
-    public Curso() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Digite el nombre del curso: ");
-        this.nombre = sc.nextLine();
+    public boolean setCodigo(String codigo) {
+        if (codigo.length() != 4) { 
+            return false; 
+        }
+        this.codigo = codigo;
+        return true;
+    } 
+
+    //setter
+    public void setNombre(String nombre) { 
+        this.nombre = nombre; 
+    }
+    
+    public void setDocente(Docente docente) {
+        this.docente = docente;
     }
 
-    // Constructor que recibe el nombre directamente
-    public Curso(String nombre) {
-        this.nombre = nombre;
+    //getter
+    public String getCodigo() {
+        return this.codigo;
     }
-
-    // Getter
+    
     public String getNombre() {
-        return nombre;
+        return this.nombre;
     }
-
-    // Setter 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-}
+    
+    //constructor
+    public Curso() {
+        
+        System.out.println("Digite el código del curso (4 dígitos):");
+        String codigoInput = sc.nextLine();
+        while (!setCodigo(codigoInput)) {
+            System.out.println("El código debe ser de 4 dígitos. Intente nuevamente:");
+            codigoInput = sc.nextLine();
+        }
+        
+        System.out.println("Digite el nombre del nuevo curso:");
+                this.nombre = sc.nextLine();
+            }
+        }
+        
