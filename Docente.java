e77826137640b6289aa9e8c339529136da41e0ee
Josue Madueño <josue4500@outlook.es>
2025-09-25 20:39:58 -0500

    Docente con comentarios

--- Docente.java
+++ Docente.java
@@ -16,6 +16,7 @@ class Docente {
         return true;
     }
 
+    // Metodo set nombres, que valida que sean solo letras o un espacio en blanco (ñ no soportada)
     public boolean setNombres (String nombres) {
         for (int i = 0; i < nombres.length(); i++) {
 	    char c = nombres.charAt(i);
@@ -26,6 +27,8 @@ class Docente {
         this.nombres = nombres;
         return true;
     }
+
+    // Metodo set apellidos, igual que el de nombres
     public boolean setApellidos (String apellidos) { 
         for (int i = 0; i < apellidos.length(); i++) {
 	   char c = apellidos.charAt(i);
@@ -36,7 +39,11 @@ class Docente {
         this.apellidos = apellidos;
         return true;
     }
+
+    // Metodo para setear especialidad, no hace falta validar nada
     public void setEspecialidad (String especialidad) { this.especialidad = especialidad; }
+
+    // Metodo para setear la experiencia, se valida que sea un numero entero
     public boolean setExperiencia (String experiencia) {
         try {
             int exp = Integer.parseInt(experiencia);
@@ -47,21 +54,26 @@ class Docente {
         }
     }
 
+    // Los getters
     public String getDni () { return this.dni; }
     public String getNombres () { return this.nombres; }
     public String getApellidos () { return this.apellidos; }
     public String getEspecialidad () { return this.especialidad; }
     public int getExperiencia () { return this.experiencia; }
 
+    // Constructor
     public Docente() {
+        // Solicitamos el DNI y no se avanza hasta que sea valido
         System.out.print("Digite el dni del nuevo docente: ");
         String dni = sc.next();
         while (!setDni(dni)) {
             System.out.print("Ese no es un dni válido, intente de nuevo: ");
             dni = sc.next();
         }
+	// Limpiamos el buffer
 	sc.nextLine();
 
+	// Solicitamos los nombres, y damos vueltas hasta que sean validos
         System.out.print("Digite los nombres del docente: ");
         String nombres = sc.nextLine();
         while (!setNombres(nombres)) {
@@ -69,6 +81,7 @@ class Docente {
             nombres = sc.nextLine();
         }
 
+	// Solicitamos los apellidos y, si, otra vez damos vueltas hasta que sea valido
         System.out.print("Digite los apellidos del docente: ");
         String apellidos = sc.nextLine();
         while (!setNombres(apellidos)) {
@@ -76,9 +89,11 @@ class Docente {
             apellidos = sc.nextLine();
         }
 
+	// Para la especialidad siempre es valido :D
         System.out.print("Digite su especialidad: ");
         setEspecialidad(sc.nextLine());
         
+	// Solicitamos los años de experiencia, ya sabes ... vueltas... valido...
         System.out.print("Digite sus años de experiencia: ");
         String experiencia = sc.next();
         while (!setExperiencia(experiencia)) {
